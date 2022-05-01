e90a41c14d1dd5afc765263a688d25a8efdcfbcc
Ryan <rcgore602@gmail.com>
2022-05-01 17:42:43 -0400

    Update index.js with proper best-of-5 code Update index.js with input verification Add scoreboard

--- index.js
+++ index.js
@@ -14,46 +14,79 @@ function computerPlay() {
 
 function userPlay() {
     let playerSelection = prompt("Enter 'Rock', 'Paper', or 'Scissors': ").toLowerCase();
+    console.log(playerSelection);
+    if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+        while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+            playerSelection = prompt("Your input is not recognized. Please enter 'Rock', 'Paper', or 'Scissors': ").toLowerCase();
+        }
+    }
     return playerSelection;
 }
 
-
+//If user wins, return 1, if computer wins, return 0.
 
 function playRound(compSelection,playerSelection) {
     if (playerSelection == 'rock') {
         if (compSelection == 'scissors') {
-            return console.log("You Win! Rock beats Scissors!");
+            console.log("You Win! Rock beats Scissors!");
+            return 1;
         } else if (compSelection == 'paper') {
-            return console.log("You Loser! Paper beats Rock!");
+            console.log("You Loser! Paper beats Rock!");
+            return 0;
         } else {
             console.log("You both selected Rock! Try again.");
-            game();
+            return 2;
         }
    }if (playerSelection == 'paper') {
         if (compSelection == 'rock') {
-            return console.log("You Win! Paper beats Rock!");
+            console.log("You Win! Paper beats Rock!");
+            return 1;
         } else if (compSelection == 'scissors') {
-            return console.log("You Loser! Scissors beat Paper!");
+            console.log("You Loser! Scissors beat Paper!"); 
+            return 0;
         } else {
             console.log("You both selected Paper! Try again.");
-            game();
+            return 2;
         }
    }if (playerSelection == 'scissors') {
         if (compSelection == 'paper') {
-            return console.log("You Win! Scissors beat Paper!");
+            console.log("You Win! Scissors beat Paper!");
         } else if (compSelection == 'rock') {
-            return console.log("You Loser! Rock beats Scissors!");
+            console.log("You Loser! Rock beats Scissors!");
         } else {
             console.log("You both selected Scissors! Try again.");
-            game();
+            return 2;
         }
     }
 }
 
+let compScore = 0;
+let playerScore = 0;
+
 function game() {
-    let compSelection = computerPlay();
-    let playerSelection = userPlay();
-    playRound(compSelection,playerSelection);
+    for (let i=0; i<5; i){
+        let compSelection = computerPlay();
+        let playerSelection = userPlay();
+        let round = playRound(compSelection,playerSelection);
+        if (round == 0) {
+            compScore = compScore+1;
+            if (compScore >= 3){
+                console.log("Game Over! The Computer WON!");
+                break;
+            }
+        } else if (round == 1) {
+            playerScore++;
+            if (playerScore >= 3) {
+                console.log("Congratulations! You WON!!!!");
+                break;
+            }
+        } else if (round == 2) {
+            i--;
+        }
+        console.log(`Score is User| ${playerScore} to ${compScore} |Computer`);
+        i++;
+    }
+    return; 
 }
 
 game();
